ad9d0ae4333098644a8b47f3467ee47e6c93c253
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-08-12 18:10:12 +1000

    add 2 apps random quote machine and weather today

---
+++ random-quote/js/index.js
@@ -0,0 +1,117 @@
+/**
+ * Code By Guillaume Caggia
+ */
+
+var currentQuote = -1;
+var tabQuote = [];
+
+var tabColors = [];
+
+tabColors[0]  = ["turquoise",     "#1ABC9C"];
+tabColors[1]  = ["green-sea",     "#16A085"];
+tabColors[2]  = ["emerland",      "#2ECC71"];
+tabColors[3]  = ["nephritis",     "#27AE60"];
+tabColors[4]  = ["peter-river",   "#3498DB"];
+tabColors[5]  = ["belize-hole",   "#2980B9"];
+tabColors[6]  = ["amethyst",      "#9B59B6"];
+tabColors[7]  = ["wisteria",      "#8E44AD"];
+tabColors[8]  = ["wet-asphalt",   "#34495E"];
+tabColors[9]  = ["midnight-blue", "#2C3E50"];
+tabColors[10] = ["sun-flower",    "#F1C40F"];
+tabColors[11] = ["orange",        "#F39C12"];
+tabColors[12] = ["carrot",        "#E67E22"];
+tabColors[13] = ["pumpkin",       "#D35400"];
+tabColors[14] = ["alizarin",      "#E74C3C"];
+tabColors[15] = ["pomegranate",   "#C0392B"];
+tabColors[16] = ["clouds",        "#ECF0F1"];
+tabColors[17] = ["silver",        "#BDC3C7"];
+tabColors[18] = ["concrete",      "#95A5A6"];
+tabColors[19] = ["asbestos",      "#7F8C8D"];
+  
+
+/**
+ * The purpose of this function is to get a random quote from the API 
+ * by a POST request with jQuery ajax function
+ */
+function getQuoteFromAPI(flagDrop) {
+
+  $.ajax({
+    type: "POST",
+    url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
+    headers: {
+    "X-Mashape-Key":
+      "j57cNxpMSLmshVySWMfmpUo3Kmrkp1wmNSajsn50PQvUDByiQQ",
+    Accept: "application/json",
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+    success: function(response) {
+      tabQuote.push(JSON.parse(response));
+      currentQuote++;
+      changeQuote(currentQuote, flagDrop);
+    }
+  });
+
+}
+
+/**
+ * This function aims to change the quote and the background after
+ * a user clicks on the buttons
+ */
+function changeQuote(iQuote, flagDrop) {
+
+  if(tabQuote[iQuote].quote.length >= 95) {
+    $('blockquote').css('font-size', '24px');
+  } else {
+    $('blockquote').css('font-size', '28px');
+  }
+
+  var color = Math.floor(Math.random() * tabColors.length);
+
+  $('body').animate({
+    "background-color": tabColors[color][1],
+
+  }, 1000);
+  
+  if (flagDrop) {
+    $(".block-quote").effect("drop", 1000, function() {
+      
+      $("#quote-text").html(tabQuote[iQuote].quote);
+      $("#quote-author").html(tabQuote[iQuote].author);
+      $(this).removeAttr( "style" ).hide().fadeIn();
+    });
+  } else {
+    $("#quote-text").html(tabQuote[iQuote].quote);
+    $("#quote-author").html(tabQuote[iQuote].author);
+  }
+  
+  
+  
+
+
+}
+
+
+$(document).ready(function() {
+  
+  //First init of the quote
+  getQuoteFromAPI(false);
+  
+  
+  $("#next-quote").click(function(){
+    if (currentQuote === tabQuote.length - 1) {
+      getQuoteFromAPI(true);
+    } else {
+      currentQuote++;
+      changeQuote(currentQuote, true);
+    }
+  });
+  
+  $("#last-quote").click(function(){
+    if (currentQuote !== 0) {
+      currentQuote--;
+      changeQuote(currentQuote);
+    }
+    
+  });
+  
+});
---
+++ weather-today/js/index.js
@@ -0,0 +1,105 @@
+$(document).ready(main());
+
+var obj = {};
+
+function getIP(callback) {
+  $.getJSON('http://ipinfo.io', function(data){ 
+    obj.IP = data;
+    if (callback)
+      callback();
+  });
+}
+
+function getLocation(callback) {
+  $.getJSON('http://ip-api.com/json/' + obj.IP.ip, function(data) {
+    obj.location = data;
+    if (callback)
+      callback();
+  });
+}
+
+function getWeather(callback) {
+  var lat   = obj.location.lat;
+  var lon   = obj.location.lon;
+  var id    = "c9d2b40305897ca8dc04f4904b1b1a5f";
+  var units = "metric";
+
+  $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' 
+    + lat + '&lon=' + lon + '&APPID=' + id + '&units=' + units,
+    function(data) {
+      obj.weather = data;
+      if (callback)
+        callback();
+    });
+}
+
+function updateView() {
+
+  console.log(obj);
+
+  var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+  var dt = new Date();
+  var strDt = (n(dt.getDay())) + 
+    ' ' + monthNames[dt.getMonth()] + ' ' + dt.getFullYear();
+
+  var sunriseDt = new Date(obj.weather.sys.sunrise * 1000);
+  var sunsetDt  = new Date(obj.weather.sys.sunset * 1000);
+
+  $('#dateToday').text(strDt);
+  $('#city').text(obj.location.city);
+  $('#country').text(obj.location.country);
+  $('#w-temp').text(Math.round(obj.weather.main.temp) +'°C');
+  $('#w-cond').text(obj.weather.weather[0].description);
+
+  $('#sunrise').text(n(sunriseDt.getHours()) + 'h' 
+    + n(sunriseDt.getMinutes()) );
+  $('#sunset').text(n(sunsetDt.getHours()) + 'h' 
+    + n(sunsetDt.getMinutes()) ) ;
+
+
+  switch(obj.weather.weather[0].main.toLowerCase()) {
+    case 'dizzle':
+        $('.icon-weather').empty();
+        $('#snow').clone().appendTo('.icon-weather');
+        break;
+      case 'clouds':
+        $('.icon-weather').empty();
+        $('#clouds').clone().appendTo('.icon-weather');
+        break;
+      case 'rain':
+        $('.icon-weather').empty();
+        $('#rain').clone().appendTo('.icon-weather');
+        break;
+      case 'snow':
+        $('.icon-weather').empty();
+        $('#snow').clone().appendTo('.icon-weather');
+        break;
+      case 'clear':
+        $('.icon-weather').empty();
+        $('#clear').clone().appendTo('.icon-weather');
+        break;
+      case 'thunderstorm':
+        $('.icon-weather').empty();
+        $('#thunderstorm').clone().appendTo('.icon-weather');
+        break;
+  }
+
+}
+
+function n(n){
+    return n > 9 ? "" + n : "0" + n;
+}
+
+function main() {
+
+  async.series([
+    getIP,
+    getLocation,
+    getWeather,
+    updateView
+  ]);  
+
+}
